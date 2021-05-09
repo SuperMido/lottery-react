bcd03ff94b35131e28383b0c72d0c255d513f377
SuperMido <supermido1996@gmail.com>
2021-05-09 11:27:31 +0700

    Pick a winner

--- src/App.js
+++ src/App.js
@@ -3,22 +3,77 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 // import web3 from "./web3";
 import lottery from "./lottery";
+import web3 from "./web3";
 
 function App() {
   const [manager, setManager] = useState('');
+  const [players, setPlayers] = useState([]);
+  const [balance, setBalance] = useState('');
+  const [value, setValue] = useState('');
+  const [message, setMessage] = useState('');
 
   async function ContractData() {
-    const managerId = await lottery.methods.manager().call();
-    setManager(managerId)
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    setManager(manager)
+    setPlayers(players);
+    setBalance(balance);
   }
 
   useEffect(() => {
     ContractData();
   }, []);
+
+  const playerEnter = async () => {
+
+    const accounts = await web3.eth.getAccounts();
+
+    setMessage('Waiting on transaction successfully...');
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(value, 'ether')
+    });
+
+    setMessage('You have been entered successfully!')
+  };
+
+  const pickWinner = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    setMessage('Waiting on transaction successfully...')
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0],
+    });
+    
+    setMessage('A winner has ben picked!');
+  };
+
   return (
     <div className="App">
       <h2>Lottery Contract</h2>
       <p>This contract is managed by {manager}</p>
+      <p>There are currently {players.length} people entered, competing to win {web3.utils.fromWei(balance, 'ether')} ether!</p>
+      <hr></hr>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount of ether to enter</label>
+          <input
+            value = { value }
+            onChange = { event =>  setValue(event.target.value)}
+          />
+        </div>
+        <button onClick={ () => playerEnter() }>Enter</button>
+      <hr/>
+      <h1>
+        {message}
+      </h1>
+      <hr />
+      <h4>Ready to pick a winner?</h4>
+      <button onClick = { () => pickWinner() }>Pick a Winner</button>
     </div>
   );
 }
